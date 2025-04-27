b4a59ada2433b4b450450dfb3236a0b6d543b1f6
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2025-04-27 15:05:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="cwv-scanner",
     version="1.0.0",
     author="SirCryptic",
-    author_email="sircryptic@protonmail.com",
     description="Common Web Application Vulnerability Scanner",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sircryptic/cwv-scanner",
-    packages=find_packages(),  # Automatically find cwv_scanner package
+    packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "cwv_scanner": ["waf_indicators.json", "vulnerabilities.json", "user_agents.txt", "banners/*.txt"],
-    },
     install_requires=[
-        "requests>=2.28.0",
-        "fake-useragent>=0.1.11",
-        "tabulate>=0.8.9",
+        "requests",
+        "fake-useragent",
+        "tabulate",
+        "beautifulsoup4",
     ],
-    entry_points={
-        "console_scripts": [
-            "cwv-scanner=cwv_scanner.cwv_scanner:main",
-        ],
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
+    entry_points={
+        "console_scripts": [
+            "cwv-scanner=cwv_scanner.cwv_scanner:main",
+        ],
+    },
 )
